88c58c289f929c14f8fb5f5f8cf30ccdecc5d27f
Chris F <chrisfirmani@hotmail.com>
2021-02-06 15:03:37 -0500

    added red led and refactor led turn off

--- raspberry-pi/temperature_sensor.py
+++ raspberry-pi/temperature_sensor.py
@@ -6,23 +6,33 @@ import Adafruit_DHT
 import firebase_admin
 from firebase_admin import firestore, credentials
 
+# Global Variables and initial setup
 USER_NAME = 'Eve'
 
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 14
 
+green_led = 7
+red_led = 10
+led_list = [green_led, red_led]
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(green_led, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(red_led, GPIO.OUT, initial=GPIO.LOW)
 
 cred = credentials.Certificate('ServiceAccountKey.json')
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
 
+def turn_all_LED_off(led_list: list):
+	for i in led_list:
+		GPIO.output(i, GPIO.LOW)
+
 while True:
-	GPIO.output(7, GPIO.HIGH)
 	humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
 	if temperature is not None:
+		GPIO.output(green_led, GPIO.HIGH)
 		print(f"Temp = {temperature}, Hum = {humidity}")
 
 		date_time = datetime.now()
@@ -40,8 +50,9 @@ while True:
 			u'user': USER_NAME
 		})
 	else:
+		GPIO.output(red_led, GPIO.HIGH)
 		print("Sensor failed")
 	
 	sleep(5)
-	GPIO.output(7, GPIO.LOW)
+	turn_all_LED_off(led_list)
 	sleep(25)
