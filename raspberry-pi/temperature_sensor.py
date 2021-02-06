4a66af293680c3682682a8c169c1e59ca66e39b8
Chris F <chrisfirmani@hotmail.com>
2021-02-06 12:51:36 -0500

    Added firestore foor python

--- raspberry-pi/temperature_sensor.py
+++ raspberry-pi/temperature_sensor.py
@@ -1,18 +1,31 @@
 from time import sleep
-import requests
+from datetime import datetime
+# import requests
 import Adafruit_DHT
-
-r = requests.get("https://reqres.in/api/users?page=2")
-
-print(r.status_code)
+import firebase_admin
+from firebase_admin import firestore, credentials
 
 DHT_SENSOR = Adafruit_DHT.DHT11
 DHT_PIN = 14
+
+cred = credentials.Certificate('ServiceAccountKey.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 while True:
 	humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
 	if temperature is not None:
 		print(f"Temp = {temperature}, Hum = {humidity}")
+
+		date_time = date_time.now()
+
+		doc_ref = db.collection(u'test').document(u'testdate')
+		doc_ref.set({
+			u'temperature': temperature,
+			u'humidity': humidity
+			u'time': date_time
+		})
 	else:
 		print("Sensor failed")
-	sleep(3)
+	
+	sleep(10)
