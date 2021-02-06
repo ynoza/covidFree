e7655a5abe56bffaae9f229e6f562aaf7b46e45d
Setu Potnis <setupotnis@gmail.com>
2021-02-06 01:10:22 -0500

    remove drawer. add new data for table. change title of components.

--- covid-free/src/components/Deposits.js
+++ covid-free/src/components/Deposits.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import Link from '@material-ui/core/Link';
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Title from './Title';
+import React from "react";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Title from "./Title";
 
 function preventDefault(event) {
   event.preventDefault();
@@ -18,9 +18,9 @@ export default function Deposits() {
   const classes = useStyles();
   return (
     <React.Fragment>
-      <Title>Recent Deposits</Title>
+      <Title>Average Temperature</Title>
       <Typography component="p" variant="h4">
-        $3,024.00
+        24° C
       </Typography>
       <Typography color="textSecondary" className={classes.depositContext}>
         on 15 March, 2019
