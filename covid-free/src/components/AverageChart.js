f9e0873ec22abae0361e0d5070f68e1426024785
Setu Potnis <setupotnis@gmail.com>
2021-02-06 16:32:32 -0500

    add tooltip to average chart

--- covid-free/src/components/AverageChart.js
+++ covid-free/src/components/AverageChart.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import { useTheme } from '@material-ui/core/styles';
-import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
-import moment from 'moment';
-import Title from './Title';
-import { temperature } from './Orders';
-import { tempObj } from './Deposits';
+import React from "react";
+import { useTheme } from "@material-ui/core/styles";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Label,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
+import moment from "moment";
+import Title from "./Title";
+import { temperature } from "./Orders";
+import { tempObj } from "./Deposits";
 // Generate Sales Data
-function createData(date, amount) {
-  return { date, amount };
+function createData(date, Temperature) {
+  return { date, Temperature };
 }
 
 let len = tempObj.length;
 let averageTemp = new Array(len);
 let dates = Object.keys(tempObj);
-for(let k of dates) {
+for (let k of dates) {
   // console.log(k);
-    let todayArr = tempObj[k];
-    const sum = todayArr.reduce((a, b) => a + b, 0);
-    const avg = sum / todayArr.length || 0;
-    averageTemp.push(createData(k.slice(0, 4), avg));
+  let todayArr = tempObj[k];
+  const sum = todayArr.reduce((a, b) => a + b, 0);
+  const avg = sum / todayArr.length || 0;
+  averageTemp.push(createData(k.slice(0, 4), avg));
 }
 averageTemp.shift();
-console.log(averageTemp);
-
 
 export default function AverageChart() {
   const theme = useTheme();
@@ -40,23 +46,34 @@ export default function AverageChart() {
             left: 24,
           }}
         >
-          <XAxis 
-           dataKey={'date'}
-           stroke={theme.palette.text.secondary}>
-            <Label dy={20}>
-             Day
-            </Label>
+          <XAxis dataKey={"date"} stroke={theme.palette.text.secondary}>
+            <Label dy={20}>Day</Label>
           </XAxis>
           <YAxis stroke={theme.palette.text.secondary}>
             <Label
               angle={270}
               position="left"
-              style={{ textAnchor: 'middle', fill: theme.palette.text.primary }}
+              style={{ textAnchor: "middle", fill: theme.palette.text.primary }}
             >
               Tempeature (°C)
             </Label>
           </YAxis>
-          <Line type="monotone" dataKey="amount" stroke={theme.palette.primary.main} dot={true} />
+          <Tooltip
+            labelStyle={{ color: "black" }}
+            itemStyle={{ color: "black" }}
+            formatter={(value, name) => {
+              return `${value} °C`;
+            }}
+            labelFormatter={(value) => {
+              return `Day: ${value}`;
+            }}
+          />
+          <Line
+            type="monotone"
+            dataKey="Temperature"
+            stroke={theme.palette.primary.main}
+            dot={true}
+          />
         </LineChart>
       </ResponsiveContainer>
     </React.Fragment>
