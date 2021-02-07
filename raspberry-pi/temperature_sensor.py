2ef098ed3c875aae432200697d69531d634fc4ef
Chris F <chrisfirmani@hotmail.com>
2021-02-06 21:26:30 -0500

    refactor and commented

--- raspberry-pi/temperature_sensor.py
+++ raspberry-pi/temperature_sensor.py
@@ -21,6 +21,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(green_led, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(red_led, GPIO.OUT, initial=GPIO.LOW)
 
+# Firebase init
 cred = credentials.Certificate('ServiceAccountKey.json')
 app = firebase_admin.initialize_app(cred)
 db = firestore.client()
@@ -29,18 +30,18 @@ def turn_all_LED_off(led_list: list):
 	for i in led_list:
 		GPIO.output(i, GPIO.LOW)
 
-counter = 0
-while counter < 10:
+while True:
 	humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
+
+	# If no temperature, do not push to db
 	if temperature is not None:
 		GPIO.output(green_led, GPIO.HIGH)
 		print(f"Temp = {temperature}, Hum = {humidity}")
 
-		# date_time = datetime.now()
-		date_time = datetime.now() - timedelta(days=7)
+		date_time = datetime.now()
 
-		current_date = date_time.strftime("%a %b %d, %Y")
-		current_time = date_time.strftime("%H:%M:%S")
+		current_date = date_time.strftime("%a %b %d, %Y") 	# Sat Feb 06, 2021
+		current_time = date_time.strftime("%H:%M:%S")		# 20:50:41
 
 		doc_ref = db.collection(USER_NAME).document(u'date')
 		doc_ref.set({
@@ -48,25 +49,15 @@ while counter < 10:
 				current_time: {
 					u'temperature': temperature,
 					u'humidity': humidity,
-					u'user': USER_NAME,
 					u'datetime': date_time
 				}
 			}
 		}, merge=True)
-		
-		# Old method
-		# day_ref = doc_ref.collection(u'times').document(current_time)
-		# day_ref.set({
-		# 	u'temperature': temperature,
-		# 	u'humidity': humidity,
-		# 	u'time': current_time,
-		# 	u'user': USER_NAME
-		# })
-		counter += 1
+
 	else:
 		GPIO.output(red_led, GPIO.HIGH)
 		print("Sensor failed")
 	
-	sleep(5)
+	sleep(2)
 	turn_all_LED_off(led_list)
-	sleep(5)
+	sleep(3)
